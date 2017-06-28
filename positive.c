f29494bcebd7ea32ea28fec85e5592669d0842e8
jayasuriyacj <jayasuriyac070@gmail.com>
2017-06-28 14:50:12 +0530

    Update positive.c

--- positive.c
+++ positive.c
@@ -1,1 +1,16 @@
-bgfhfgfg
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("\n enter a value");
+scanf("%d",&n);
+if(n>1)
+printf("positive");
+else if(n==0)
+printf("0");
+else
+printf("negative");
+getch();
+}
