487aa1d6b024298d3a12dcebb35113041e74a7c5
jayasuriyacj <jayasuriyac070@gmail.com>
2019-07-02 15:48:11 +0530

    Create holiday.py

---
+++ holiday.py
@@ -0,0 +1,12 @@
+x1='monday'
+x2='tuesday'
+x3='wednesay'
+x4='thursday'
+x5='friday'
+x6='saturday'
+x7='sunday'
+a1=str(input())
+if(a1==x1 or a1==x2 or a1==x3 or a1==x4 or a1==x5):
+     print("no")
+else:
+     print("yes")
